2fbd2ef1bbc876690b46f2cdbff916f322171e0a
Ben A <ben@sol3.me>
2022-12-01 13:27:55 +0000

    Part 2 WIP

--- ConsoleAppScratchPad/AdventOfCode2022/AdventTests.cs
+++ ConsoleAppScratchPad/AdventOfCode2022/AdventTests.cs
@@ -1,13 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace AdventOfCode2022;
 
 public class AdventTests
 {
-
     [Test]
-    public void Day1()
+    public void Day1_Part1()
     {
         var puzzleInput = new Day1CalorieCounting();
         var elfNumber = 1;
@@ -38,6 +39,37 @@ public class AdventTests
         }
 
         Console.WriteLine("HighestElfCalories : " + highestElfCalories);
+        Assert.AreEqual(highestElfCalories, 69310);
+    }
+    
+    [Test]
+    public void Day1_Part2()
+    {
+        var puzzleInput = new Day1CalorieCounting();
+        var elfNumber = 1;
+        var allElfCalories = new List<int>();
+        var currentElfCalories = 0;
+        var totalLines = puzzleInput.Calories.Count;
+        for (var index = 0; index < puzzleInput.Calories.Count; index++)
+        {
+            var line = puzzleInput.Calories[index];
+
+            if (int.TryParse(line, out var parsedInt))
+            {
+                currentElfCalories += parsedInt;
+            }
+            
+            if (string.IsNullOrEmpty(line) || index + 1 == totalLines)
+            {
+                allElfCalories.Add(currentElfCalories);
+
+                Console.WriteLine("Total For Elf #" + elfNumber + " : " + currentElfCalories);
+                currentElfCalories = 0;
+                elfNumber++;
+            }
+        }
+
+        Console.WriteLine("HighestElfCalories : " + allElfCalories.MaxBy(x => x));
         Assert.Pass();
     }
 }
