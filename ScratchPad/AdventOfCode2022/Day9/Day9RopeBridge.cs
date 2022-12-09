7119c13db2134ab3aa7ddab46ca451315a0a175c
Ben A <ben@sol3.me>
2022-12-09 13:44:18 +0000

    Day 9 part 1

--- ScratchPad/AdventOfCode2022/Day9/Day9RopeBridge.cs
+++ ScratchPad/AdventOfCode2022/Day9/Day9RopeBridge.cs
@@ -38,7 +38,23 @@ public class Day9RopeBridge
             X -= distance;
             return this;
         }
+
+        public bool IsAdjacentTo(Coord otherCoord)
+        {
+            if (Math.Abs(X - otherCoord.X) > 1)
+            {
+                return false;
+            }
+
+            if (Math.Abs(Y - otherCoord.Y) > 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
     };
+
     public record Motion(string Direction, int Distance);
 
     public Day9RopeBridge()
@@ -56,7 +72,7 @@ public class Day9RopeBridge
             .ToList();
     }
 
-    public static List<Motion> RopeMotions;
+    public List<Motion> RopeMotions;
 }
 
 [TestFixture]
@@ -72,16 +88,130 @@ public class Day9
         // the head (H) and tail (T) must always be touching (diagonally adjacent and even overlapping both count as touching):
         // Assume the head and the tail both start at the same position, overlapping.
         // How many positions does the tail of the rope visit at least once?
-        var numberOfPositionsTailMoved = 0;
-        var puzzleInput = Day9RopeBridge.RopeMotions;
+        var tailLocations = new List<Tuple<int, int>>();
+        var puzzleInput = new Day9RopeBridge();
         var headLocation = new Coord(0, 0);
         var tailLocation = new Coord(0, 0);
-        foreach (var (direction, distance) in puzzleInput)
+        foreach (var (direction, distance) in puzzleInput.RopeMotions)
         {
-            
+            var travelled = 0;
+            // Initial location
+            tailLocations.Add(new(tailLocation.X, tailLocation.Y));
+            while (travelled < distance)
+            {
+                if (direction == "U")
+                {
+                    headLocation.GoUp(1);
+                    if (tailLocation != headLocation && !tailLocation.IsAdjacentTo(headLocation))
+                    {
+                        if (tailLocation.X == headLocation.X) // In the same column
+                        {
+                            tailLocation.GoUp(1);
+                        }
+                        else
+                        {
+                            if (tailLocation.X > headLocation.X)
+                            {
+                                tailLocation.GoLeft(1);
+                            }
+                            else
+                            {
+                                tailLocation.GoRight(1);
+                            }
+
+                            tailLocation.GoUp(1);
+                        }
+
+                        tailLocations.Add(new(tailLocation.X, tailLocation.Y));
+                    }
+                }
+
+                if (direction == "D")
+                {
+                    headLocation.GoDown(1);
+                    if (tailLocation != headLocation && !tailLocation.IsAdjacentTo(headLocation))
+                    {
+                        if (tailLocation.X == headLocation.X) // In the same column
+                        {
+                            tailLocation.GoDown(1);
+                        }
+                        else
+                        {
+                            if (tailLocation.X > headLocation.X)
+                            {
+                                tailLocation.GoLeft(1);
+                            }
+                            else
+                            {
+                                tailLocation.GoRight(1);
+                            }
+
+                            tailLocation.GoDown(1);
+                        }
+
+                        tailLocations.Add(new(tailLocation.X, tailLocation.Y));
+                    }
+                }
+
+                if (direction == "R")
+                {
+                    headLocation.GoRight(1);
+                    if (tailLocation != headLocation && !tailLocation.IsAdjacentTo(headLocation))
+                    {
+                        if (tailLocation.Y == headLocation.Y) // On the same row
+                        {
+                            tailLocation.GoRight(1);
+                        }
+                        else
+                        {
+                            if (tailLocation.Y > headLocation.Y)
+                            {
+                                tailLocation.GoDown(1);
+                            }
+                            else
+                            {
+                                tailLocation.GoUp(1);
+                            }
+
+                            tailLocation.GoRight(1);
+                        }
+
+                        tailLocations.Add(new(tailLocation.X, tailLocation.Y));
+                    }
+                }
+
+                if (direction == "L")
+                {
+                    headLocation.GoLeft(1);
+                    if (tailLocation != headLocation && !tailLocation.IsAdjacentTo(headLocation))
+                    {
+                        if (tailLocation.Y == headLocation.Y) // On the same row
+                        {
+                            tailLocation.GoLeft(1);
+                        }
+                        else
+                        {
+                            if (tailLocation.Y > headLocation.Y)
+                            {
+                                tailLocation.GoDown(1);
+                            }
+                            else
+                            {
+                                tailLocation.GoUp(1);
+                            }
+
+                            tailLocation.GoLeft(1);
+                        }
+
+                        tailLocations.Add(new(tailLocation.X, tailLocation.Y));
+                    }
+                }
+
+                travelled++;
+            }
         }
 
-        numberOfPositionsTailMoved.Should().Be(13);
+        var numberOfPositionsTailMoved = tailLocations.Distinct().Count();
+        numberOfPositionsTailMoved.Should().Be(5930);
     }
 }
-
