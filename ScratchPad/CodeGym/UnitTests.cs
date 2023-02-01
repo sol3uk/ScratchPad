44cace31086767269b4a4e157ae6bff6ded60174
Ben A <ben@sol3.me>
2023-02-01 15:10:30 +0000

    Train Kata (not started) & Time kata (un-refactored)

--- ScratchPad/CodeGym/UnitTests.cs
+++ ScratchPad/CodeGym/UnitTests.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using NUnit.Framework;
 
 namespace CodeGym;
@@ -20,7 +21,76 @@ public class Tests
         };
 
         AnagramKata.GetWordsOutput(AnagramKata.WordInput, actualWordsOutput);
-        
+
         Assert.AreEqual(expectedWordsOutput, actualWordsOutput);
+    }
+
+    [Test]
+    public void InglenookShuntingTests()
+    {
+        // https://asoscom.atlassian.net/wiki/spaces/CHAS/pages/3911647492/Inglenook+Shunting+Puzzle
+        /*The puzzle consists of:
+        a main line track (A) that can hold 3 carriages in the left section and 5 in the right (B)
+        two sidings each can hold 3 carriages (C and D)
+        one locomotive, which is small enough to fit in each section of track along with the stated carriages
+        8 carriages, each given a letter, in an arbitrary arrangement on the mainline & sidings 
+         */
+        var trackA = new Stack<string>(3) // Limit of 3
+        {
+        };
+        var trackB = new Stack<string>(5); // Limit of 5
+        trackB.Push("e");
+        trackB.Push("d");
+        trackB.Push("c");
+        trackB.Push("b");
+        trackB.Push("a");
+
+        var trackC = new Stack<string>(3); // Limit of 3
+        trackC.Push("g");
+        trackC.Push("f");
+
+        var trackD = new Stack<string>(3); // Limit of 3
+        trackD.Push("h");
+
+        var mainTargetTrackState = new Stack<string>();
+        mainTargetTrackState.Push("e");
+        mainTargetTrackState.Push("d");
+        mainTargetTrackState.Push("c");
+        mainTargetTrackState.Push("b");
+        mainTargetTrackState.Push("h");
+
+        var actualMoves =
+            InglenookShuntingKata.GetMovesFromTarget(trackA, trackB, trackC, trackD, mainTargetTrackState);
+
+        var expectedMoves = new List<string>()
+        {
+            "B to C +1",
+            "D to B +1",
+        };
+        Assert.AreEqual(expectedMoves, actualMoves);
+    }
+
+    [TestCase(0,"Midnight")]
+    [TestCase(60,"One minute past midnight")]
+    [TestCase(53700,"Five minutes to three")]
+    [TestCase(53708 ,"Four minutes and fifty two seconds to three")]
+    [TestCase(1800,"Half past midnight")]
+    public void FullEnglishTimeTests(int secondsSinceMidnight, string expectedOutput)
+    {
+        // https://asoscom.atlassian.net/wiki/spaces/CHAS/pages/3928425370/Full+English+Time
+        /*The puzzle consists of:
+         You are given the number of seconds elapsed since midnight 
+            (12:00 am, 00:00, one minute passed 23:59, however you want to represent that).
+            Output Examples
+            0 : Midnight
+            60 : One minute past midnight
+            553700 : Five minutes to three
+            53708  : Four minutes and fifty two seconds to three
+         */
+        
+        var actualOutput = FullEnglishTimeKata.GetEnglishTime(secondsSinceMidnight);
+
+
+        Assert.AreEqual(expectedOutput, actualOutput);
     }
 }
--- ScratchPad/CodeGym/Usings.cs
+++ ScratchPad/CodeGym/Usings.cs
@@ -30,3 +30,165 @@ public class AnagramKata
         }
     }
 }
+
+public class InglenookShuntingKata
+{
+    public static List<string> GetMovesFromTarget(Stack<string> trackA, Stack<string> trackB, Stack<string> trackC,
+        Stack<string> trackD, Stack<string> mainTargetTrackState)
+    {
+        return new List<string>()
+        {
+            "B to C +1",
+            "D to B +1",
+        };
+    }
+}
+
+public class FullEnglishTimeKata
+{
+    public static string GetEnglishTime(int secondsSinceMidnight)
+    {
+        var minutesFull = (secondsSinceMidnight / 60);
+        var minutesRounded = (int)Math.Floor((decimal) minutesFull);
+        var hoursFull = minutesFull / 60;
+        var hoursRounded = (int)Math.Floor((decimal) hoursFull);
+        var isMorning = hoursRounded <= 11 && minutesRounded < 60;
+        var hoursNon24 = isMorning ? hoursRounded : hoursRounded - 12;
+        var minutesMinusHours = (int)Math.Floor((decimal) (minutesRounded - (hoursRounded * 60)));
+        var minutesToHour = 60 - minutesMinusHours;
+
+        if (secondsSinceMidnight == 0)
+        {
+            return "Midnight";
+        }
+
+        if (secondsSinceMidnight < 60)
+        {
+            return $"{UppercaseFirst(NumberToWords(secondsSinceMidnight))} seconds since midnight";
+        }
+
+        if (hoursRounded == 0)
+        {
+            // Midnight cases
+            // half past 
+            if (minutesMinusHours == 30)
+            {
+                return $"Half past midnight";
+            }
+                
+            // n minutes from "hour" 
+            if (minutesMinusHours < 30)
+            {
+                return $"{UppercaseFirst(NumberToWords(minutesRounded))} minute past midnight";
+            }
+                
+            // n minutes to "hour"
+            if (minutesMinusHours < 60)
+            {
+                return $"{UppercaseFirst(NumberToWords(minutesToHour))} minute to midnight";
+            }
+        }
+        else
+        {
+            if (minutesFull == minutesRounded)
+            {
+                // There are no seconds involved
+
+                // half past 
+                if (minutesMinusHours == 30)
+                {
+                    return $"half past";
+                }
+                
+                // n minutes from "hour" 
+                if (minutesMinusHours < 30)
+                {
+                    return $"{UppercaseFirst(NumberToWords(minutesMinusHours))} minute from {UppercaseFirst(NumberToWords(hoursRounded))}";
+                }
+                
+                // n minutes to "hour"
+                if (minutesMinusHours < 60)
+                {
+                    var nextHourComingUp = hoursNon24 + 1;
+                    if (minutesToHour > 1)
+                    {
+                        return $"{UppercaseFirst(NumberToWords(minutesToHour))} minutes to {NumberToWords(nextHourComingUp)}";
+                    }
+                    else
+                    {
+                        return $"{UppercaseFirst(NumberToWords(minutesToHour))} minute to {NumberToWords(nextHourComingUp)}"; 
+                    }
+                }
+            }
+
+            if (minutesRounded > 0)
+            {
+                return "bad";
+            }
+        }
+
+
+
+        return "bad";
+    }
+    
+    public static string NumberToWords(int number)
+    {
+        if (number == 0)
+            return "zero";
+
+        if (number < 0)
+            return "minus " + NumberToWords(Math.Abs(number));
+
+        string words = "";
+
+        if ((number / 1000000) > 0)
+        {
+            words += NumberToWords(number / 1000000) + " million ";
+            number %= 1000000;
+        }
+
+        if ((number / 1000) > 0)
+        {
+            words += NumberToWords(number / 1000) + " thousand ";
+            number %= 1000;
+        }
+
+        if ((number / 100) > 0)
+        {
+            words += NumberToWords(number / 100) + " hundred ";
+            number %= 100;
+        }
+
+        if (number > 0)
+        {
+            if (words != "")
+                words += "and ";
+
+            var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+            if (number < 20)
+                words += unitsMap[number];
+            else
+            {
+                words += tensMap[number / 10];
+                if ((number % 10) > 0)
+                    words += "-" + unitsMap[number % 10];
+            }
+        }
+
+        return words;
+    }
+    
+    static string UppercaseFirst(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return string.Empty;
+        }
+        char[] a = s.ToCharArray();
+        a[0] = char.ToUpper(a[0]);
+        return new string(a);
+    }
+}
